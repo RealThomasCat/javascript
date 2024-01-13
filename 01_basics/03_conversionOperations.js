f9c128eb19fcf3980efb5470383c1c09d1751bca
Yashveer Singh <110716311+RealThomasCat@users.noreply.github.com>
2024-01-13 14:47:16 +0000

    tricky operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -51,11 +51,52 @@ let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
 
 let isLoggedIn3 = ""
 let booleanIsLoggedIn3 = Boolean(isLoggedIn3)
-console.log(typeof booleanIsLoggedIn3) // o/ => boolean
-console.log(booleanIsLoggedIn3) // o/p => false
+// console.log(typeof booleanIsLoggedIn3) // o/ => boolean
+// console.log(booleanIsLoggedIn3) // o/p => false
 
 // TAKEAWAY (conversion to boolean):
     // 1 => true
     // 0 => true
     // "yes" => true
     // "" => false
+
+// ********** OPERATIONS **********
+
+let value = 3
+let negValue = -value // Converting number to negative
+// console.log(negValue)
+
+// BASIC MATH OPERATIONS
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // power
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = "yashveer"
+
+let str3 = str1 + str2 // Basic sting concatenation
+// console.log(str3) // o/p => helloyashveer
+
+// COMPLEX CONCATENATION
+// console.log("1" + "2") // o/p => 12
+// console.log(1 + "2") // o/p => 12
+// console.log("1" + 2) // o/p => 12
+// console.log("1" + 2 + 2) // o/p => 122
+// console.log(1 + 2 + "2") // o/p => 32 *** IMPORTANT ***
+
+// console.log(+true) // o/p => 1
+// console.log(true+) // Error
+// console.log(+"") // o/p => 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 // all asigned 4
+// console.log(num2)
+
+let x = 1;
+let y = x++; // Increases after usage
+console.log(x,y) // o/p => 2 1
+let a = 1;
+let b = ++a; // Increases before usage
+console.log(a,b) // o/p => 2 2
